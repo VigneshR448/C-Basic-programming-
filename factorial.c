0ea522e67b838712d1cfe1527d446c6725f90f8e
Vignesh Ramesh <vigneshramesh448@gmail.com>
2025-06-05 14:48:00 +0530

    Create factorial.c
    factorial the given number eg:5*4*3*2*1=120

---
+++ factorial.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n,i=1,fact=1;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    while(i<=n)
+    {
+        fact=fact*i;
+        i++;   
+    }
+    printf("Factorial No:%d",fact);
+}
