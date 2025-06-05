86fdada365f9921462fb5456ba2a9e1ef4a4c42c
Vignesh Ramesh <vigneshramesh448@gmail.com>
2025-06-05 12:19:39 +0530

    Update Character is upper , lower or number.c
    checking the character is upper ,lower,number or symbol in c

--- Character is upper , lower or number.c
+++ Character is upper , lower or number.c
@@ -1,1 +1,23 @@
-
+#include<stdio.h>
+int main()
+{
+  char ch;
+printf("Enter the Character:%d",ch);
+scanf("%d",&ch);
+if(ch>='a' && ch<='z')
+{
+printf("Lower");
+}
+else if(ch>='A' && ch<='z')
+{
+printf("Upper");
+}
+else if(ch>='1' && ch>='0')
+  {
+printf("Number");
+  }
+else
+{
+printf("Symbol");
+}
+}
