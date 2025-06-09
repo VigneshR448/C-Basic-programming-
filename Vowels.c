2576456eb8db04625b376e03ce0b7f78527b60a9
Vignesh Ramesh <vigneshramesh448@gmail.com>
2025-06-09 09:26:19 +0530

    Create Vowels.c
    to print vowel letters in a string

---
+++ Vowels.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+     char a[100];
+    int i;
+    printf("Enter the String:");
+    scanf("%s",a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]=='a' || a[i]=='e' || a[i]=='i'||a[i]=='o' ||a[i]=='u' ||a[i]=='A' ||a[i]=='E' || a[i]=='I' ||a[i]=='O' ||a[i]=='U')
+        {
+            printf("%c",a[i]);
+        }
+    }
+    return 0;
+}
