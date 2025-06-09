1378941add70ffdafa5996db5989e014d8261881
Vignesh Ramesh <vigneshramesh448@gmail.com>
2025-06-09 11:10:12 +0530

    Create 2D-Even Addition.c

---
+++ 2D-Even Addition.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int r,c,sum=0;
+    int i,j;
+    printf("Enter the row and column:");
+    scanf("%d %d",&r,&c);
+    int arr[r][c];
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            printf("Enetr the matrics value:");
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        if(arr[i][j]%2==0){
+        sum=sum+arr[i][j];
+        }
+    }
+     printf("%d ",sum);
+     return 0;
+}
